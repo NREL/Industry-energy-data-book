ec689b635a9177481efd834177dbfe6048036bc7
calmc <cmcmill@gmail.com>
2019-07-01 17:19:08 -0400

    farm counts comments

--- ag.py
+++ ag.py
@@ -18,7 +18,7 @@ fuel oil) are included.
 # aebn: agricultural expenses by NAICS #######################################
 """
 Automatically collect state-level agricultural expense data by NAICS
-code from USDA NASS 2017 Census results. 
+code from USDA NASS 2017 Census results.
 """
 
 base_url = 'http://quickstats.nass.usda.gov/api/api_GET/'
@@ -36,11 +36,11 @@ r = requests.get(base_url, params=params)
 #print(r.content)
 url = r.url
 #print(url)
-    
+
 response = urllib.request.urlopen(url)
 data = response.read()
 datajson = json.loads(data)
-aebn = pd.DataFrame(datajson['data'], 
+aebn = pd.DataFrame(datajson['data'],
                     columns=['state_name','state_alpha','state_ansi',
                              'domaincat_desc','Value'])
 
@@ -51,54 +51,54 @@ aebn[['NAICS','c']] = aebn.b.str.split(")", expand=True)
 aebn = aebn.drop(['domaincat_desc','a','b','c'], axis=1)
 #print(aebn.head(20))
 
-    
+
 ####### Remove invalid values & Rename columns & Set index & Sort
 invalid = '                 (D)'
 aebn = aebn.replace(invalid, aebn.replace([invalid], '0'))
-aebn.rename(columns = {'state_name':'state', 
-                       'state_alpha':'state_abbv', 
-                       'Value':'ag_expense_$'}, 
+aebn.rename(columns = {'state_name':'state',
+                       'state_alpha':'state_abbv',
+                       'Value':'ag_expense_$'},
                        inplace=True)
 aebn.set_index('state', inplace=True)
 aebn = aebn.sort_index(ascending=True)
 #print(aebn.head(50))
-    
-    
+
+
 ####### Remove commas in numbers
 aebn['ag_expense_$'] = aebn['ag_expense_$'].apply(lambda x: x.replace(
         ',', "")).astype(int)
 
-    
+
 ####### Find fraction by state
 aebn['ag_expense_state_pct'] = aebn['ag_expense_$'].divide(
                                aebn['ag_expense_$'].sum(level='state'))
 #aebn.to_csv('ag_expenses_by_naics_000dollars.csv')
 #print(aebn.head(50))
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
 # ep: electricity price ##################################################
 """
-Automatically collect 2017 annual electricity sales (MWh) and revenues 
-(000 $) data by state and calculate electricity prices ($/kWh) in each 
-state. 
+Automatically collect 2017 annual electricity sales (MWh) and revenues
+(000 $) data by state and calculate electricity prices ($/kWh) in each
+state.
 """
-    
-####### Collect electricity revenue data (000 $) 
+
+####### Collect electricity revenue data (000 $)
 rev = pd.read_excel(
         'http://www.eia.gov/electricity/data/state/revenue_annual.xlsx',
         header=1, nrows=51)
-    
+
 rev = rev[['State','Industrial']]
 rev.rename(columns = {'State':'state_abbv',
                       'Industrial':'rev_000$'}, inplace = True)
 #print(rev)
-    
-    
-####### Collect electricity sales data (MWh) 
+
+
+####### Collect electricity sales data (MWh)
 sal = pd.read_excel(
         'http://www.eia.gov/electricity/data/state/sales_annual.xlsx',
          header=1, nrows=51)
@@ -107,8 +107,8 @@ sal = sal[['State','Industrial']]
 sal.rename(columns = {'State':'state_abbv',
                       'Industrial':'sal_mwh'}, inplace = True)
 #print(sal)
-    
-    
+
+
 ####### Calculate electricity price ($/kWh)
 merge = pd.merge(sal, rev, on='state_abbv')
 merge['ep_kwh'] = merge['rev_000$']/merge['sal_mwh']
@@ -129,7 +129,7 @@ P474eafd3e12544e19338a00227af3001_2_252iT0R0x17
 """
 
 ee_source = pd.read_excel('ag_SOURCE_electricity_expenses.xlsx',
-                          sheet_name=18, 
+                          sheet_name=18,
                           header=5,
                           usecols="B:C"
                           )                                                       #API?
@@ -146,8 +146,8 @@ ee['state'] = ee['state'].str.upper()
 
 # elec: electricity use ######################################################
 """
-Calculate 2017 agricultural sector electricity use (MMBtu) by NAICS code in 
-each state based on aebn (the share of a subsector's ag expenses in a state's 
+Calculate 2017 agricultural sector electricity use (MMBtu) by NAICS code in
+each state based on aebn (the share of a subsector's ag expenses in a state's
 total ag expenses), ee (each state's electricity expenses, 000$), and ep
 (each state's electricity price, $/kWh).
 """
@@ -169,8 +169,8 @@ elec = elec[['state_abbv', 'NAICS', 'elec_mmbtu']]
 # fc: farm counts ############################################################
 
 """
-Automatically collect county-level farm counts data by NAICS from USDA NASS 
-2017 Census results. 
+Automatically collect county-level farm counts data by NAICS from USDA NASS
+2017 Census results.
 """
 
 base_url = 'http://quickstats.nass.usda.gov/api/api_GET/'
@@ -188,11 +188,11 @@ r = requests.get(base_url, params=params)
 #print(r.content)
 url = r.url
 #print(url)
-    
+
 response = urllib.request.urlopen(url)
 data = response.read()
 datajson = json.loads(data)
-fc = pd.DataFrame(datajson['data'], 
+fc = pd.DataFrame(datajson['data'],
                   columns=['state_name','state_alpha','county_name',
                            'domaincat_desc','Value'])
 
@@ -203,14 +203,14 @@ fc[['NAICS','c']] = fc.b.str.split(")", expand=True)
 fc = fc.drop(['domaincat_desc','a','b','c'], axis=1)
 #print(fc.head(20))
 
-    
+
 ####### Remove invalid values & Rename columns & Set index & Sort
 invalid = '                 (D)'
 fc = fc.replace(invalid, fc.replace([invalid], '0'))
-fc.rename(columns = {'state_name':'state', 
-                     'state_alpha':'state_abbv', 
+fc.rename(columns = {'state_name':'state',
+                     'state_alpha':'state_abbv',
                      'Value':'farm_counts',
-                     'county_name':'county'}, 
+                     'county_name':'county'},
                      inplace=True)
 fc.set_index('state', inplace=True)
 fc = fc.sort_index(ascending=True)
@@ -218,7 +218,7 @@ fc.to_csv('ag_farm_counts.csv')
 print(fc.head(20))
 
 
-####### Remove observations for NAICS 1119, which double counts observations 
+####### Remove observations for NAICS 1119, which double counts observations
 ####### for 11192 and "11193 & 11194 & 11199".
 fc = pd.read_csv('ag_farm_counts.csv')                                                  # Only for test
 fc = fc[fc.NAICS != '1119']
@@ -235,22 +235,32 @@ fc_state = pd.pivot_table(fc, values = ['farm_counts'], index = ['state'],
                     columns = ['NAICS'], aggfunc = np.sum
                     )
 
+
+#
 for s in fc_state.index:
-    
+
     for n in fc_state.columns.levels[1]:
-        
+
         i = fc[(fc.index == s) & (fc['NAICS'] == n)].index
-        
+
+        # each i is empty, with size == 0. fc has an integer index (fc.index),
+        #whereas fc_state has a string index of states (fc_state.index).
+        # I'd also suggest learning how to use pandas concat, join, and merge
+        # (https://pandas.pydata.org/pandas-docs/stable/user_guide/merging.html)
+        # as opposed to looping through each row of a dataframe. I started
+        # out using a lot of loops, but after I got the hang of of merging, etc.
+        # I realized how much faster it was.
+        print(i, i.size)
+
         if i.size == 0:
             pass
-        
+
         else:
+
+            # This equation should be dividing fc.farm_counts, not fc.NAICS
+            # fc_state also has a multiindex for its coluns based on
+            # ('farm_counts', n), not ('NAICS', n): fc_state.columns.levels
             fc.loc[i,'statefraction'] = fc[fc.state == s].NAICS / fc_state.loc[
                     s, ('NAICS', n)]
-            
+
 print(fc.head(10))
-
-
-
-        
-       
